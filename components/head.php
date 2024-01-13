07464514cbe8c8e85711b886ef3660a9b5a1724e
duclong09 <duclongsew@gmail.com>
2024-01-13 22:42:47 +0700

    issue: header upper

--- components/head.php
+++ components/head.php
@@ -1,3 +1,7 @@
+<title>UI Kit</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>UI Kit</title>
+<link rel="stylesheet" href="resources/css/uikit.min.css" />
+<link rel="stylesheet" href="resources/style.css" />
+<script src="resources/js/uikit.min.js"></script>
+<script src="resources/js/uikit-icons.min.js"></script>
