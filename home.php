99e351cd493dcb9d5359c9a5250c1e00829629d7
duclong09 <duclongsew@gmail.com>
2024-01-13 21:07:14 +0700

    base source ui kit

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once 'components/head.php' ?>
+</head>
+<body>
+    <?php require_once 'components/header.php' ?>
+    <main>
+        
+    </main>
+    <?php require_once 'components/footer.php' ?>
+    <?php require_once 'components/script.php' ?>
+</body>
+</html>
