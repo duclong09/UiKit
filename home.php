94e9b3ad18da4f7a79acaf68ccfc7802491fd7bb
duclong09 <duclongsew@gmail.com>
2024-01-19 09:02:32 +0700

    coming swpper slide, grid images

--- home.php
+++ home.php
@@ -10,7 +10,7 @@
     <main id="homepage">
         <div class="panel-banner">
             <div class="uk-container uk-container-1440">
-                <div uk-grid>
+                <div class="uk-grid-small" uk-grid>
                     <div class="uk-width-2-3@m">
                         <div class="panel-slide">
                             <div class="swiper-container">
@@ -19,19 +19,19 @@
                                 <div class="swiper-wrapper">
                                     <div class="swiper-slide">
                                         <div class="slide-item">
-                                            <span class="image img-scaledown">
+                                            <span class="image img-cover">
                                                 <img src="resources/img/slider-1.png" alt="">
                                             </span>
-                                            <div class="slide-overlay">
-                                                <div class="slide-title">
+                                            <div class="content-overlay">
+                                                <div class="overlay-title">
                                                     The TV <br>
                                                     that changes <br>
                                                     everything
                                                 </div>
-                                                <div class="slide-description">
+                                                <div class="overlay-description">
                                                     Start from $399
                                                 </div>
-                                                <div class="slide-readmore">
+                                                <div class="overlay-readmore">
                                                     <a href="#" class="btn-readmore btn">
                                                         <span>Learn more</span>
                                                         <span class="uk-icon" uk-icon="chevron-right"></span>
@@ -42,18 +42,18 @@
                                     </div>
                                     <div class="swiper-slide">
                                         <div class="slide-item">
-                                            <span class="image img-scaledown">
+                                            <span class="image img-cover">
                                                 <img src="resources/img/slider-2.png" alt="">
                                             </span>
-                                            <div class="slide-overlay">
-                                                <div class="slide-title">
+                                            <div class="content-overlay">
+                                                <div class="overlay-title">
                                                     Blast past <br>
                                                     fats. <br>
                                                 </div>
-                                                <div class="slide-description">
+                                                <div class="overlay-description">
                                                     Start from $399
                                                 </div>
-                                                <div class="slide-readmore">
+                                                <div class="overlay-readmore">
                                                     <a href="#" class="btn-readmore btn">
                                                         <span>Learn more</span>
                                                         <span class="uk-icon" uk-icon="chevron-right"></span>
@@ -64,19 +64,19 @@
                                     </div>
                                     <div class="swiper-slide">
                                         <div class="slide-item">
-                                            <span class="image img-scaledown">
+                                            <span class="image img-cover">
                                                 <img src="resources/img/slider-3.png" alt="">
                                             </span>
-                                            <div class="slide-overlay">
-                                                <div class="slide-title">
+                                            <div class="content-overlay">
+                                                <div class="overlay-title">
                                                     Heavy on <br>
                                                     features <br>
                                                     Light on price
                                                 </div>
-                                                <div class="slide-description">
+                                                <div class="overlay-description">
                                                     Start from $399
                                                 </div>
-                                                <div class="slide-readmore">
+                                                <div class="overlay-readmore">
                                                     <a href="#" class="btn-readmore btn">
                                                         <span>Learn more</span>
                                                         <span class="uk-icon" uk-icon="chevron-right"></span>
@@ -91,7 +91,49 @@
                         </div>
                     </div>
                     <div class="uk-width-1-3@m">
-                        123
+                        <div class="banner-wrapper">
+                            <div class="banner-item">
+                                <a href="#" class="image img-cover">
+                                    <img src="resources/img/img4-1.jpg" alt="">
+                                </a>
+                                <div class="content-overlay">
+                                    <div class="overlay-title">
+                                        Sale up to 30% <br>
+                                        DRONE <br>
+                                        PHOTOGRAPHY
+                                    </div>
+                                    <div class="overlay-description">
+                                        Start from $399
+                                    </div>
+                                    <div class="overlay-readmore">
+                                        <a href="#" class="btn-readmore btn">
+                                            <span>Shop now</span>
+                                            <span class="uk-icon" uk-icon="chevron-right"></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="banner-item">
+                                <a href="#" class="image img-cover">
+                                    <img src="resources/img/img4-2.jpg" alt="">
+                                </a>
+                                <div class="content-overlay">
+                                    <div class="overlay-title">
+                                        BEAST <br>
+                                        HEADPHONE
+                                    </div>
+                                    <div class="overlay-description">
+                                        Start from $399
+                                    </div>
+                                    <div class="overlay-readmore">
+                                        <a href="#" class="btn-readmore btn">
+                                            <span>Shop now</span>
+                                            <span class="uk-icon" uk-icon="chevron-right"></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
